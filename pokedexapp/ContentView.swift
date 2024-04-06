e614d9501ad6143aa8c099ecd305046409f35660
Miguel Tomairo <mtomairom@falabella.com.pe>
2024-04-05 22:49:51 -0500

    Initial Commit

---
+++ pokedexapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pokedexapp
+//
+//  Created by miguel tomairo on 5/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pokedexapp/pokedexappApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexappApp.swift
+//  pokedexapp
+//
+//  Created by miguel tomairo on 5/04/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
